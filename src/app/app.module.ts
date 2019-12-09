b98b41fcbe50088fff0d4e267bbcd4b89308c52e
shahzaibaziz <shahzaibaziz6@gmail.com>
2019-12-09 15:08:17 +0500

    FYp project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistorComponent } from './components/registor/registor.component';
+import { FyplistComponent } from './components/fyplist/fyplist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    RegistorComponent,
+    FyplistComponent
   ],
   imports: [
     BrowserModule,
